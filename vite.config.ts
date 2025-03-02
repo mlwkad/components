23ad2ad118283289ccec43c190320eb157dbc9b3
zzz <3502219532@qq.com>
2025-03-02 20:17:41 +0800

    666

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import compression from 'vite-plugin-compression'
 export default defineConfig({
   plugins: [
     //npm install rollup-plugin-visualizer --save-dev
-    visualizer({ open: true }),  //打包分析
+    visualizer({ open: true }),  //打包结果分析
     vue(),
     AutoImport({  //8
       resolvers: [ElementPlusResolver()],  //10.自动导入组件
@@ -20,17 +20,20 @@ export default defineConfig({
     Components({  //9
       resolvers: [ElementPlusResolver()]  //10.自动按需加载组件
     }),
-    compression({  //npm i vite-plugin-compression --save-dev  gzip压缩
-      threshold: 10240,  //10kb
-      algorithm: 'gzip'
-    })
+    // compression({  //npm i vite-plugin-compression --save-dev  gzip压缩
+    //   threshold: 10240,  //10kb
+    //   algorithm: 'gzip'
+    // })
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src')  
     }
   },
   //代理设置
+  //npm i gh-pages -D  静态网页部署
+  //"deploy": "gh-pages -d build",
+  //先npm run build,将dist改名成build 再npm run deploy  获得新分支gh-pages
   server: {
     proxy: {
       '/api': 'http://testapi.xuexiluxian.cn'  //其他请求为/api/...时,被代理成http://testapi.xuexiluxian.cn/api/...
@@ -47,9 +50,9 @@ export default defineConfig({
         //       console.log(id)  //其他默认打包到
         //     }
         //   },
-        manualChunks: {
-          'chunk-vendors': ['vue', 'vue-router', 'pinia', 'axios', 'element-plus', '@vueuse/core', 'vue-i18n']
-        }
+        // manualChunks: {
+        //   'chunk-vendors': ['vue', 'vue-router', 'pinia', 'axios', 'element-plus', '@vueuse/core', 'vue-i18n']
+        // }
       }
     }
   }
